b7df57ff54a8cbb3f7dc1d49366cbb563faf78f6
Sheharoze <sheharozuddin@gmail.com>
2024-11-05 13:37:31 -0500

    Added Mongo URL

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const itemsrouter = require('./routes/itemrequest-routes');
@@ -6,15 +7,15 @@ const airportrouter = require('./routes/airportPickup-routes');
 
 
 const app = express();
+const port  = Number(process.env.PORT) || 6000;
+const mongoURL = process.env.MONGO_URL || "";
 
 app.use(express.json());
 app.use("/api" , itemsrouter);
 app.use("/api" , airportrouter);
 
-mongoose.connect(
-    "mongodb+srv://homewithoutbordersfl:sheharoz123@hwbcluster.qgtje.mongodb.net/?retryWrites=true&w=majority&appName=hwbcluster"
-).then(()=>app.listen(5000,()=>{
-    console.log("Connected to MongoDb and Listening on Port 5000");
+mongoose.connect(mongoURL).then(()=>app.listen(port,()=>{
+    console.log(`Connected to MongoDb and Listening on Port ${port}`);
 })).catch((err)=>{
     console.log(err);
 })
